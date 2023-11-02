67a8e6f0f1550b0f05b5bf45244b9a4f4e1d12c9
Ars Vladimirov <grokhael@gmail.com>
2023-11-02 15:49:02 +0600

    change preview

--- src/settings.py
+++ src/settings.py
@@ -39,7 +39,7 @@ HOMEPAGE_URL: str = (
 )
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 7520575
+PREVIEW_IMAGE_ID: int = 7520523
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/supervisely-hrda-plants-demo"
